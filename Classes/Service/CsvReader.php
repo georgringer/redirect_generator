c8edee5715a0f756257ec0bddb8ca6b5a1a15a8f
Georg Ringer <georgringer@users.noreply.github.com>
2021-04-26 10:01:01 +0200

    Update CsvReader.php

--- Classes/Service/CsvReader.php
+++ Classes/Service/CsvReader.php
@@ -557,9 +557,9 @@ class CsvReader
 
         // walk specific depth finding posssible delimiter characters
         for ($i = 0; $i < $strlen; $i++) {
-            $ch = $data{$i};
-            $nch = (isset($data{$i + 1})) ? $data{$i + 1} : false;
-            $pch = (isset($data{$i - 1})) ? $data{$i - 1} : false;
+            $ch = $data[$i];
+            $nch = (isset($data[$i + 1])) ? $data[$i + 1] : false;
+            $pch = (isset($data[$i - 1])) ? $data[$i - 1] : false;
 
             // open and closing quotes
             if ($ch == $enclosure) {
@@ -670,16 +670,16 @@ class CsvReader
 
         // force the parser to process end of data as a character (false) when
         // data does not end with a line feed or carriage return character.
-        $lch = $data{$strlen - 1};
+        $lch = $data[$strlen - 1];
         if ($lch != "\n" && $lch != "\r") {
             $strlen++;
         }
 
         // walk through each character
         for ($i = 0; $i < $strlen; $i++) {
-            $ch = (isset($data{$i})) ? $data{$i} : false;
-            $nch = (isset($data{$i + 1})) ? $data{$i + 1} : false;
-            $pch = (isset($data{$i - 1})) ? $data{$i - 1} : false;
+            $ch = (isset($data[$i])) ? $data[$i] : false;
+            $nch = (isset($data[$i + 1])) ? $data[$i + 1] : false;
+            $pch = (isset($data[$i - 1])) ? $data[$i - 1] : false;
 
             // open/close quotes, and inline quotes
             if ($ch == $this->enclosure) {
@@ -707,9 +707,9 @@ class CsvReader
                     $current .= $ch;
                     $i++;
                 } elseif ($nch != $this->delimiter && $nch != "\r" && $nch != "\n") {
-                    for ($x = ($i + 1); isset($data{$x}) && ltrim($data{$x}, $white_spaces) == ''; $x++) {
+                    for ($x = ($i + 1); isset($data[$x]) && ltrim($data[$x], $white_spaces) == ''; $x++) {
                     }
-                    if ($data{$x} == $this->delimiter) {
+                    if ($data[$x] == $this->delimiter) {
                         $enclosed = false;
                         $i = $x;
                     } else {
@@ -1062,7 +1062,7 @@ class CsvReader
         if ($value !== null && $value != '') {
             $delimiter_quoted = preg_quote($delimiter, '/');
             $enclosure_quoted = preg_quote($this->enclosure, '/');
-            if (preg_match("/" . $delimiter_quoted . "|" . $enclosure_quoted . "|\n|\r/i", $value) || ($value{0} == ' ' || substr($value, -1) == ' ') || $this->enclose_all) {
+            if (preg_match("/" . $delimiter_quoted . "|" . $enclosure_quoted . "|\n|\r/i", $value) || ($value[0] == ' ' || substr($value, -1) == ' ') || $this->enclose_all) {
                 $value = str_replace($this->enclosure, $this->enclosure . $this->enclosure, $value);
                 $value = $this->enclosure . $value . $this->enclosure;
             }
